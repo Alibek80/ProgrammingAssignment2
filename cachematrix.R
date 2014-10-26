88ba22ce2defc804d0fcaba66e637be5edca4ce9
Alibek <alibek80@gmail.com>
2014-10-26 21:15:02 +0600

    Commiting the inverse matrix solve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that caches a matrix and its inverse matrix
+## 
 
-## Write a short comment describing this function tes
-
-
-
+## makeCacheMatrix function that creates list that contains matrix and its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve solves an inverse matrix if the inverse matrix was not previously calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)  ## Return a matrix that is the inverse of 'x'
+        }
+        matrix<-x$get
+        m<-solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
